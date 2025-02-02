9cdfb21068974fe9e980e5543a6c34dc49842046
java <salehabdullaict@gmail.com>
2025-02-02 17:00:15 +0300

    AddTwoNumbers

--- addTwoNumber2/src/ListNode.java
+++ addTwoNumber2/src/ListNode.java
@@ -1,9 +1,10 @@
-public class ListNode {
+class ListNode {
     int val;
     ListNode next;
 
-    public ListNode(int val) {
+    public ListNode(int val){
         this.val = val;
         this.next = null;
     }
+    
 }
--- addTwoNumber2/src/Main.java
+++ addTwoNumber2/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         // Example 1: 342 + 465 = 807
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
+        ListNode l1 = new ListNode(7);
+        l1.next = new ListNode(8);
+        l1.next.next = new ListNode(9);
 
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(2);
+        l2.next.next = new ListNode(3);
 
         ListNode result = addTwoNumbers(l1, l2);
 
@@ -16,13 +16,16 @@ public class Main {
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
         ListNode dummyHead = new ListNode(0);
         ListNode p = l1, q = l2, curr = dummyHead;
+
         int carry = 0;
 
         while (p != null || q != null) {
             int x = (p != null) ? p.val : 0;
             int y = (q != null) ? q.val : 0;
+
             int sum = carry + x + y;
             carry = sum / 10;
             curr.next = new ListNode(sum % 10);
@@ -31,13 +34,12 @@ public class Main {
             if (p != null) p = p.next;
             if (q != null) q = q.next;
         }
-
         if (carry > 0) {
             curr.next = new ListNode(carry);
         }
-
         return dummyHead.next;
     }
+
 
     public static void printNode(ListNode node) {
         while (node != null) {
@@ -46,6 +48,5 @@ public class Main {
         }
         System.out.println(); // To move to the next line after printing the linked list
     }
+
 }
-
-
