8830c3977a8b98132022d36493a7fb04808f6a65
java <salehabdullaict@gmail.com>
2025-02-04 09:11:36 +0300

    Cylinder

---
+++ Cylinder/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3.75);
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getArea());
+    }
+}
