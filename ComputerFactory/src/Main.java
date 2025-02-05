eeafd18108d07f5ab2545c75cea3710c9c17985a
java <salehabdullaict@gmail.com>
2025-02-05 14:28:35 +0300

    Composion compared to inheritance

--- ComputerFactory/src/Main.java
+++ ComputerFactory/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        ComputerCase computerCase = new ComputerCase("2208", "Asus", "240");
+        Monitor monitor = new Monitor("27inches beast", "Acer", 27, "2540 x 1440");
+        Motherboard motherboard = new Motherboard("BJ-200", "Asus",
+                4, 6, "v2.44");
+        PersonalComputer personalComputer = new PersonalComputer("2025", "Asus",
+                computerCase, monitor, motherboard);
+
+//        personalComputer.getComputerCase().pressPowerButton();
+//        personalComputer.getMonitor().drawPixelAt(10, 10, "red");
+//        personalComputer.getMotherboard().loadProgram("IntelliJ");
+
+        personalComputer.powerUp();
+
+
     }
 }
