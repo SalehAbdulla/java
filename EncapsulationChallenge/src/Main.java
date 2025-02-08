93015f76d75b34d35324bcd206d755a004d68c51
java <salehabdullaict@gmail.com>
2025-02-08 22:15:04 +0300

    Update

--- EncapsulationChallenge/src/Main.java
+++ EncapsulationChallenge/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Printer printer = new Printer(50, true);
+        System.out.println(printer.getPagesPrinted());
+        printer.printPages(5);
+        System.out.println(printer.getPagesPrinted());
     }
 }
--- EncapsulationChallenge/src/Printer.java
+++ EncapsulationChallenge/src/Printer.java
@@ -5,22 +5,37 @@ public class Printer {
     private boolean duplex;
 
 
-    public Printer() {
-        tonerLevel = 100;
-        duplex = false;
-        pagesPrinted = 0;
+    public Printer(int tonerLevel, boolean duplex) {
+        this.pagesPrinted = 0;
+        this.tonerLevel = (tonerLevel >= 0 && tonerLevel <= 100) ? tonerLevel : -1;
+        this.duplex = duplex;
     }
 
     public int addToner(int tonerAmount){
-        tonerLevel += tonerAmount;
-        if (tonerLevel > 100 || tonerLevel < 0){
+        int tempAmount = tonerAmount + tonerLevel;
+        if (tempAmount > 100 || tempAmount < 0){
             return -1;
         }
+        tonerLevel += tonerAmount;
         return tonerLevel;
     }
 
     public int printPages(int pages){
-
+        int jobPages =  (duplex) ? (pages / 2) + (pages % 2) : pages;
+        pagesPrinted += jobPages;
+        System.out.println("It's a duplex printer");
+        return jobPages;
     }
 
+    public int getTonerLevel() {
+        return tonerLevel;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+
+    public boolean isDuplex() {
+        return duplex;
+    }
 }
