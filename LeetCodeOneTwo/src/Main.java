7b9a829bd6d1f8a32c19b4650365baa73c95735a
java <salehabdullaict@gmail.com>
2025-02-12 20:09:07 +0300

    I'm The One

---
+++ LeetCodeOneTwo/src/Main.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(Solution.twoSum(new int[]{2,7,11,15} ,9)));
+    }
+}
---
+++ LeetCodeOneTwo/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    //Input: nums = [2,7,11,15], target = 9   |||| 7 -> 0
+    //Output: [0,1]
+    //Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+    public static int[] twoSum(int[] nums, int target){
+
+        Map<Integer, Integer> complements = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer complementIndex = complements.get(nums[i]);
+
+            if (complementIndex != null){
+                return new int[] {complementIndex, i};
+            }
+
+            complements.put(target - nums[i], i);
+        }
+
+        return nums;
+    }
+}
