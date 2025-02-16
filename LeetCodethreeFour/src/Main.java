5ecd67d26c5b553bcb767cb5c5b399d1c41731a7
java <salehabdullaict@gmail.com>
2025-02-16 15:21:59 +0300

    PRACTICE

---
+++ LeetCodethreeFour/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(SolutionOne.longestSubString(s));
+        String s2 = "abcabcbb";
+        System.out.println(SolutionTwo.longestSubString(s2));
+    }
+}
