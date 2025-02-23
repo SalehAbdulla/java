125c3b54e60a0b955f0d02fbb3079e624bb66161
java <salehabdullaict@gmail.com>
2025-02-23 11:37:14 +0300

    Practice makes improvement

--- FindMedianSortedArraysTwo/src/Main.java
+++ FindMedianSortedArraysTwo/src/Main.java
@@ -3,51 +3,13 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int[] newArray = findMedianSortedArrays(new int[] {1,2}, new int[] {4,5});
-        System.out.println(Arrays.toString(newArray));
+        int[] nums1 = new int[] {1, 3, 5, 6, 7, 8, 9, 10};
+        int[] nums2 = new int[] {2, 2, 3, 4, 5, 6};
+
+        int[] solutionArray = Solution.findTheMedianOfTwoArray(nums1, nums2);
+        System.out.println(Arrays.toString(solutionArray));
 
     }
 
-    // Input: nums1 = [1,3], nums2 = [2]
-    public static int[] findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-        int[] mergedArray = new int[nums1.length + nums2.length];
-
-        int i = 0;
-        int j = 0;
-        int k = 0;
-
-        while (i < nums1.length && j < nums2.length){
-
-            if (nums1[i] < nums2[j]){
-                mergedArray[k] = nums1[i];
-                i++;
-            } else {
-                mergedArray[k] = nums2[j];
-                j++;
-            }
-
-            k++;
-        }
-
-        while (i < nums1.length){
-            mergedArray[k] = nums1[i];
-            i++;
-            k++;
-        }
-
-
-        while (j < nums2.length){
-            mergedArray[k] = nums2[j];
-            j++;
-            k++;
-        }
-
-
-
-
-        return mergedArray;
-
-    }
 
 }
--- FindMedianSortedArraysTwo/src/Solution.java
+++ FindMedianSortedArraysTwo/src/Solution.java
@@ -1,4 +1,35 @@
-public class Solution {
+import java.util.Arrays;
 
+public class Solution {
+    //Input: nums1 = [1,3], nums2 = [2]
+    //Output: 2.00000
+    //Explanation: merged array = [1,2,3] and median is 2.
+
+    public static int[] findTheMedianOfTwoArray(int[] nums1, int[] nums2){
+
+        // Let's merge the two arrays -- must be sorted;
+
+        int[] mergedArray = new int[nums1.length + nums2.length];
+
+        int k = 0;
+
+        for (int i = 0; i < nums1.length; i++){
+            mergedArray[k++] = nums1[i];
+        }
+
+        for (int j = 0; j < nums2.length; j++){
+            mergedArray[k++] = nums2[j];
+        }
+
+        Arrays.sort(mergedArray);
+
+
+        // Find the median
+
+
+
+
+        return mergedArray;
+    }
 
 }
