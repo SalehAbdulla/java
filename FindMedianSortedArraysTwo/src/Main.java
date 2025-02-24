104a08e39b0c77e554b183e75258bfacebb0c901
java <salehabdullaict@gmail.com>
2025-02-24 12:58:40 +0300

    I'm The One

--- FindMedianSortedArraysTwo/src/Main.java
+++ FindMedianSortedArraysTwo/src/Main.java
@@ -3,11 +3,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int[] nums1 = new int[] {1, 3, 5, 6, 7, 8, 9, 10};
-        int[] nums2 = new int[] {2, 2, 3, 4, 5, 6};
-
-        int[] solutionArray = Solution.findTheMedianOfTwoArray(nums1, nums2);
-        System.out.println(Arrays.toString(solutionArray));
+        double median = Solution.findTheMedianOfTwoArrays(new int[] {1, 2}, new int[] {3, 4});
+        System.out.println(median);
 
     }
 
--- FindMedianSortedArraysTwo/src/Solution.java
+++ FindMedianSortedArraysTwo/src/Solution.java
@@ -1,33 +1,47 @@
-import java.util.Arrays;
+class Solution {
 
-public class Solution {
+    //Example 1:
     //Input: nums1 = [1,3], nums2 = [2]
     //Output: 2.00000
     //Explanation: merged array = [1,2,3] and median is 2.
 
-    public static int[] findTheMedianOfTwoArray(int[] nums1, int[] nums2){
+    public static double findTheMedianOfTwoArrays(int[] array1, int[] array2) {
 
-        // Let's merge the two arrays -- must be sorted;
-
-        int[] mergedArray = new int[nums1.length + nums2.length];
+        int[] mergedArray = new int[array1.length + array2.length];
 
         int i = 0;
         int j = 0;
         int k = 0;
 
-        while (i < nums1.length && j < nums2.length){
-            if (nums1[i] < nums2[j]){
+        while (i < array1.length && j < array2.length) {
 
+            if (array1[i] < array2[j]) {
+                mergedArray[k] = array1[i];
+                i++;
+            } else {
+                mergedArray[k] = array2[j];
+                j++;
             }
+            k++;
         }
 
+        while (i < array1.length) {
+            mergedArray[k] = array1[i];
+            i++;
+            k++;
+        }
 
-        // Find the median
+        while (j < array2.length) {
+            mergedArray[k] = array2[j];
+            j++;
+            k++;
+        }
 
+        if (mergedArray.length % 2 == 1) {
+            return mergedArray[mergedArray.length / 2];
+        } else {
+            return (double) ( mergedArray[mergedArray.length / 2] + mergedArray[mergedArray.length / 2] -1 ) / 2;
+        }
 
-
-
-        return mergedArray;
     }
-
 }
