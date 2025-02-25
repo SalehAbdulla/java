eeaf9f0e32b03ca8f114e337df824ca0d92d4aab
java <salehabdullaict@gmail.com>
2025-02-25 11:38:39 +0300

    I'm The One

--- ArrayList/src/dev/lpa/Main.java
+++ ArrayList/src/dev/lpa/Main.java
@@ -7,36 +7,29 @@ record GroceryItem(String name, String type, int count){
     public GroceryItem(String name){
         this(name, "DAIRY", 1);
     }
-
-    @Override
-    public String toString() {
-        return String.format("%d %s in %s", count, name.toUpperCase(), type);
-    }
 }
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         GroceryItem[] groceryArray = new GroceryItem[3];
-        groceryArray[0] = new GroceryItem("milk");
-        groceryArray[1] = new GroceryItem("apples", "FRUIT", 6);
+        groceryArray[0] = new GroceryItem("MILK");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
         groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
-
         System.out.println(Arrays.toString(groceryArray));
 
+        System.out.println("-".repeat(30));
 
+        // if we did not explicitly specify the type of the list, then by default it will be a list of Objects
         ArrayList objectList = new ArrayList();
         objectList.add(new GroceryItem("Butter"));
-        objectList.add("Orange");
+        objectList.add("Yogurt");
 
-        ArrayList<GroceryItem> groceryList = new ArrayList<>();
-        groceryList.add(new GroceryItem("Butter"));
-        groceryList.set(0, new GroceryItem("Banana"));
-        groceryList.remove(0);
+        System.out.println("-".repeat(30));
 
-        System.out.println(groceryList);
-
-
-
+        // All elements are GroceryItem
+        ArrayList<GroceryItem> groceryItems = new ArrayList<>();
+        groceryItems.add(new GroceryItem("Milk"));
+        groceryItems.add(new GroceryItem("Butter"));
 
 
     }
