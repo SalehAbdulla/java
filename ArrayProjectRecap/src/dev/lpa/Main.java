db09515b8b967f66d0eb1312f3c05926af3d4c19
java <salehabdullaict@gmail.com>
2025-03-13 12:12:38 +0300

    Enough is Enough- no more procrastention

--- ArrayProjectRecap/src/dev/lpa/Main.java
+++ ArrayProjectRecap/src/dev/lpa/Main.java
@@ -2,30 +2,21 @@ package dev.lpa;
 
 import java.util.Arrays;
 
+record GroceryItem(String name, String type, int count) {
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+}
+
 public class Main {
-    public static void main(String[] args) {
 
-        int[] myInt = new int[10];
-        myInt[5] = 50;
-        System.out.println(Arrays.toString(myInt));
+    public static void main(String[] args){
+        Object[] groceryArray = new Object[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = "5 Oranges";
 
-
-        double[] myDouble = new double[10];
-        myDouble[3] = 3.5;
-        System.out.println(Arrays.toString(myDouble));
-        System.out.println(myDouble[3]);
-
-        int[] firstTen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println("first = " + firstTen[0]);
-
-        int firstTenArrayLength = firstTen.length;
-        System.out.println(firstTenArrayLength);
-
-
-
-
-
-
+        System.out.println(Arrays.toString(groceryArray));
 
     }
 }
