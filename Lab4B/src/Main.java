2969e7646f17bff896d1b6279e1b7761e249300b
java <salehabdullaict@gmail.com>
2025-03-18 17:20:18 +0300

    I'm MEE

---
+++ Lab4B/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(XOnEitherSides("Lexus"));
+    }
+
+    public static boolean XOnEitherSides(String text) {
+        //Write your code here
+        boolean hasChar = false;
+
+        if (Character.toLowerCase(text.charAt(0)) == 'x' || Character.toLowerCase(text.charAt(text.length() - 1))  == 'x'){
+            hasChar = true;
+        }
+
+
+        //End of your code
+        return hasChar;
+    }
+}
---
+++ Lab4B/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        int nb = 4;
+        incrementByTwo(nb);
+        System.out.println("nb = " + nb);
+    }
+    public static void incrementByTwo(int num) {
+        num = num + 2;
+    }
+}
