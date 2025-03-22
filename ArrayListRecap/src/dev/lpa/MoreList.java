be44ce147188fcb0a8f1f5b99ae11ccfd4610bd0
java <salehabdullaict@gmail.com>
2025-03-22 21:44:43 +0300

    I'm The One

--- ArrayListRecap/src/dev/lpa/MoreList.java
+++ ArrayListRecap/src/dev/lpa/MoreList.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 public class MoreList {
     public static void main(String[] args) {
+
         String[] items = {"apples", "bananas", "milk", "eggs"};
         List<String> list = List.of(items);
 
         System.out.println(list);
         System.out.println("Data type = " + list.getClass().getName());
+
 
         ArrayList<String> groceryArrayList = new ArrayList<>(list);
         groceryArrayList.add("yogurt");
@@ -19,9 +21,38 @@ public class MoreList {
         ArrayList<String> arrayListTwo = new ArrayList<>(List.of("Red", "Green", "Blue"));
         System.out.println(arrayListTwo);
 
-        // Adding or joining elements from two ArrayList
+        // Adding or joining elements - two ArrayList
         arrayListTwo.addAll(groceryArrayList);
         System.out.println(arrayListTwo);
+
+        System.out.println("Third item = " + arrayListTwo.get(2));
+
+        System.out.println("-".repeat(30));
+
+
+        groceryArrayList.add("yogurt");
+        System.out.println(groceryArrayList);
+
+
+        if (groceryArrayList.contains("yogurt")){
+            System.out.println("Grocery ArrayList contains yogurt");
+        }
+
+        System.out.println("First yogurt = " + groceryArrayList.indexOf("yogurt"));
+        System.out.println("Second yogurt = " + groceryArrayList.lastIndexOf("yogurt"));
+
+
+        System.out.println(groceryArrayList);
+
+        groceryArrayList.remove(1);
+        System.out.println(groceryArrayList);
+
+        groceryArrayList.removeAll(List.of("eggs", "yogurt"));
+        System.out.println(groceryArrayList);
+
+
+
+        
 
 
     }
