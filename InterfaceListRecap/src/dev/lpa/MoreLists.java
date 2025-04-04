dc1ec03d3ab723c224d46adb9a9f9a3e282d99cc
java <salehabdullaict@gmail.com>
2025-04-04 18:13:06 +0300

    Mental Toughness

--- InterfaceListRecap/src/dev/lpa/MoreLists.java
+++ InterfaceListRecap/src/dev/lpa/MoreLists.java
@@ -1,6 +1,8 @@
 package dev.lpa;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoreLists {
@@ -23,6 +25,80 @@ public class MoreLists {
         // Instead of adding an element each time, we can simply add it directly from the beginning
         ArrayList<String> arrayListComesWithValues = new ArrayList<>(List.of("Apple", "Orange", "Banana"));
         System.out.println(arrayListComesWithValues);
+
+        // There is another method to add all the element one time
+        ArrayList<String> usingAddAllArrayList = new ArrayList<>();
+        usingAddAllArrayList.addAll(List.of("Apple", "Orange", "Banana"));
+        System.out.println(usingAddAllArrayList);
+
+        // Array we use [], whereas Arraylist we use .get()
+        ArrayList<String> newArrayList = new ArrayList<>();
+        newArrayList.addAll(List.of("one", "two", "three"));
+
+        // Check if an element exists in an ArrayList
+        if (newArrayList.contains("one")) {
+            System.out.println("List contains one");
+        }
+
+        newArrayList.add("fourthElement");
+        newArrayList.add("fourthElement");
+
+        System.out.println(newArrayList);
+        System.out.println("First time 'fourthElement' occurs = " + newArrayList.indexOf("fourthElement"));
+        System.out.println("Last time 'fourthElement' occurs = " + newArrayList.lastIndexOf("fourthElement"));
+
+        System.out.println(newArrayList);
+
+        // remove by index
+        newArrayList.remove(1);
+
+        // remove by element name
+        newArrayList.remove("one");
+
+        // remove many elements together
+        newArrayList.removeAll(List.of("two", "three"));
+        System.out.println(newArrayList);
+
+        // retain - remove all element except what mentioned
+        newArrayList.retainAll(List.of("fourthElement"));
+        System.out.println(newArrayList);
+
+        // Clear the list clear()
+        newArrayList.clear();
+        System.out.println("ArrayList isEmpty? " + newArrayList.isEmpty());
+        System.out.println(newArrayList);
+
+        // Let's add new items
+        newArrayList.addAll(List.of("saleh", "ahmed", "mahdi", "abdulla"));
+
+        // this time add items with Arrays.asList
+        // Mutable, NOT resizable
+        newArrayList.addAll(Arrays.asList("fatima", "zainab", "zahra"));
+
+        System.out.println(newArrayList);
+
+        // Sort the elements
+        newArrayList.sort(Comparator.naturalOrder());
+        System.out.println(newArrayList);
+
+        // Convert all elements into uppercase
+        int counter = 0;
+        for (String item : newArrayList) {
+            newArrayList.set(counter, item.toUpperCase());
+            counter++;
+        }
+        System.out.println(newArrayList);
+
+        // reverse order this time
+        newArrayList.sort(Comparator.reverseOrder());
+        System.out.println(newArrayList);
+
+
+        // Convert ArrayList to Array, .toArray(new String[ArrayList.size()])
+        var newArrayListToArray = newArrayList.toArray(new String[newArrayList.size()]);
+        System.out.println(Arrays.toString(newArrayListToArray));
+
+
 
     }
 }
