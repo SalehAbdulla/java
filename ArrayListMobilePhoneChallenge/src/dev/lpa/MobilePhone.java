23e95f2a477b79c04f1635848b0cd2299382caf2
java <salehabdullaict@gmail.com>
2025-04-07 00:01:01 +0300

    I'm The One

--- ArrayListMobilePhoneChallenge/src/dev/lpa/MobilePhone.java
+++ ArrayListMobilePhoneChallenge/src/dev/lpa/MobilePhone.java
@@ -8,7 +8,7 @@ public class MobilePhone {
     ArrayList<Contacts> myContacts;
 
     //    -  A constructor that takes a String (the phone number) and initialises myNumber and instantiates myContacts.
-    public MobilePhone(String myNumber){
+    public MobilePhone(String myNumber) {
         this.myNumber = myNumber;
         myContacts = new ArrayList<>();
     }
@@ -18,8 +18,8 @@ public class MobilePhone {
     //        -  addNewContact(), has one parameter of type Contact and returns a boolean.
     //        Returns true if the contact doesn't exists, or false if the contact already exists.
 
-    public boolean addNewContact(Contacts contact){
-        if (myContacts.contains(contact)){
+    public boolean addNewContact(Contacts contact) {
+        if (myContacts.contains(contact)) {
             return false;
         } else {
             myContacts.add(contact);
@@ -28,21 +28,67 @@ public class MobilePhone {
     }
 
 
+    //        updateContact(), has two parameters of type Contact (the old contact that will be updated with the new contact) and returns a boolean.
+    //        Returns true if the contact exists and was updated successfully, or false if the contact doesn't exists.
+    public boolean updateContact(Contacts oldContact, Contacts newContact) {
+        if (myContacts.contains(oldContact)){
+            myContacts.set(myContacts.indexOf(oldContact), newContact);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    //        -  updateContact(), has two parameters of type Contact (the old contact that will be updated with the new contact) and returns a boolean. Returns true if the contact exists and was updated successfully, or false if the contact doesn't exists.
-    //
-    //        -  removeContact(), has one parameter of type Contact and returns a boolean. Returns true if the contact exists and was removed successfully, or false if the contact doesn't exists.
-    //
-    //        -  findContact(), has one parameter of type Contact and returns an int. The returned value is it's position in the ArrayList, it will either be -1 (doesn't exists) or a value greater than or equal to 0 (does exists).
-    //
+    //        -  removeContact(), has one parameter of type Contact and returns a boolean.
+    //        Returns true if the contact exists and was removed successfully, or false if the contact doesn't exists.
+
+    public boolean removeContact(Contacts contact) {
+        if (myContacts.contains(contact)){
+            myContacts.remove(contact);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    //        -  findContact(), has one parameter of type Contact and returns an int.
+    //        The returned value is it's position in the ArrayList, it will either be -1 (doesn't exists) or a value greater than or equal to 0 (does exists).
+
+    public int findContact(Contacts contact){
+        if (myContacts.contains(contact)){
+            return myContacts.indexOf(contact);
+        } else {
+            return -1;
+        }
+    }
+
     //        -  findContact(), same as above, only it has one parameter of type String.
+    public int findContact(String name){
+        if (myContacts.contains(name)){
+            return myContacts.indexOf(name);
+        } else {
+            return -1;
+        }
+    }
+
     //
-    //        -  queryContact(), has one parameter of type String and returns a Contact. Use the String to search for the name and then return the Contact. Return null otherwise.
-    //
+    //        -  queryContact(), has one parameter of type String and returns a Contact.
+    //        Use the String to search for the name and then return the Contact. Return null otherwise.
+
+    public Contacts queryContact(String name){
+        if (myContacts.contains(name)){
+            return myContacts.get(myContacts.indexOf(name));
+        } else {
+            return null;
+        }
+    }
+
+
     //        -  printContacts(), has no parameters and doesn't return anything. Print the contacts in the following format:
 
-    public void printContacts(){
-        for (int i = 0; i < myContacts.size(); i++){
+    public void printContacts() {
+        for (int i = 0; i < myContacts.size(); i++) {
             System.out.println(myContacts.get(i).name + " -> " + myContacts.get(i).number);
         }
     }
