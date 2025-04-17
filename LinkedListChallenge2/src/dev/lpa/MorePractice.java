d07a47f99fbc6d4ef9e0577e3b4699178fd0b183
java <salehabdullaict@gmail.com>
2025-04-17 22:56:24 +0300

    This is a simple LinkedList<> iteration using forEach loop.

--- LinkedListChallenge2/src/dev/lpa/MorePractice.java
+++ LinkedListChallenge2/src/dev/lpa/MorePractice.java
@@ -6,3 +6,23 @@ record Places(String name, int distance) {
         return String.format("%s (%d)", name, distance);
     }
 }
+
+public class MorePractice {
+    public static void main(String[] args) {
+
+        LinkedList<Location> locationToVisitAgain = new LinkedList<>();
+        add(new Location("Bahrain", 110), locationToVisitAgain);
+        add(new Location("Saudi", 135), locationToVisitAgain);
+        System.out.println(locationToVisitAgain);
+    }
+
+    public static void add(Location place, LinkedList<Location> list){
+        for (Location l: list) {
+            if (l.name().equalsIgnoreCase(place.name())){
+                return;
+            }
+        }
+        list.add(place);
+    }
+
+}
