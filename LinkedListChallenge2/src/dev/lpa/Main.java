8fb8ed261577a077bfcffcf5ae3b592bc4f38d3d
java <salehabdullaict@gmail.com>
2025-04-17 19:53:22 +0300

    I'm The One

--- LinkedListChallenge2/src/dev/lpa/Main.java
+++ LinkedListChallenge2/src/dev/lpa/Main.java
@@ -1,7 +1,8 @@
 package dev.lpa;
 
+import javax.swing.text.html.HTMLDocument;
+import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Scanner;
 
 record Place(String name, int distance) {
 
@@ -14,106 +15,30 @@ record Place(String name, int distance) {
 public class Main {
     public static void main(String[] args) {
         LinkedList<Place> placesToVisit = new LinkedList<>();
-        Place bahrain = new Place("Bahrain", 1234);
+        Place bahrain = new Place("Bahrain", 110);
+        Place saudi = new Place("Saudi", 135);
 
-        addPlace(placesToVisit, bahrain);
-        addPlace(placesToVisit, new Place("bahrain", 1234));
-        addPlace(placesToVisit, new Place("Brisbane", 917));
-        addPlace(placesToVisit, new Place("Perth", 3923));
-        addPlace(placesToVisit, new Place("Alice Springs", 2771));
-        addPlace(placesToVisit, new Place("Darwin", 3972));
-        addPlace(placesToVisit, new Place("Melbourne", 1234));
-        placesToVisit.addFirst(new Place("Sydney", 0));
+        addPlace(bahrain, placesToVisit);
+        addPlace(saudi, placesToVisit);
 
-        var iterator = placesToVisit.listIterator();
-        Scanner scanner = new Scanner(System.in);
-        boolean quitLoop = false, forward = true;
-
-        printMenu();
-        while (!quitLoop){
-            // This means the list we're at the start of the list
-            if (!iterator.hasPrevious()){
-                System.out.println("Originating : " + iterator.next());
-                forward = true;
-            }
-            // This means we're at the end of the list
-            if (!iterator.hasNext()){
-                System.out.println("Final : " + iterator.next());
-                forward = false;
-            }
-
-
-            System.out.println("Enter Value: ");
-            String menuItem = scanner.nextLine().toUpperCase().substring(0, 1);
-
-            switch (menuItem){
-
-                case "F":
-                    System.out.println("User wants to go forward");
-                    if(iterator.hasNext()){
-                        System.out.println(iterator.next());
-                    }
-                    break;
-                case "B":
-                    System.out.println("User wants to go backward");
-                    if(iterator.hasPrevious()){
-                        System.out.println(iterator.previous());
-                    }
-                    break;
-                case "L":
-                    System.out.println("User wants to list places");
-                    break;
-                case "M":
-                    System.out.println(placesToVisit);
-                    break;
-                default:
-                    quitLoop = true;
-                    break;
-            }
-
-        }
+        addPlace(new Place("Kuwait", 118), placesToVisit);
+        addPlace(new Place("bahrain", 110), placesToVisit);
 
         System.out.println(placesToVisit);
 
     }
 
-    private static void addPlace(LinkedList<Place> list, Place place) {
-//        if (list.contains(place)){
-//            System.out.println("Found duplicate " + place);
-//            return;
-//        }
 
-        for (Place p : list) {
-            if (p.name().equalsIgnoreCase(place.name())) {
-                System.out.println("Found Duplicate: " + place);
-            }
-            return;
-        }
+    public static void addPlace(Place place, LinkedList<Place> list){
 
-        int matchedIndex = 0;
-
-        for (var listPlace: list) {
-            if (place.distance() < listPlace.distance()){
-                list.add(matchedIndex, place);
+        for (Place p : list){
+            if (p.name().equalsIgnoreCase(place.name())){
                 return;
             }
-            matchedIndex++;
         }
 
         list.add(place);
 
     }
-
-    private static void printMenu(){
-        System.out.println("""
-                Available actions (select word or letter):
-                (F)orward
-                (B)ackwards
-                (L)ist Places
-                (M)enu
-                (Q)uit""");
-    }
-
-
 
 }
