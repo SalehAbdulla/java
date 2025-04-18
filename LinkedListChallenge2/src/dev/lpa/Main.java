dd2f220b9c782f452c62069a8fd6fecef5a27a6c
java <salehabdullaict@gmail.com>
2025-04-18 16:52:52 +0300

    I'm The One

--- LinkedListChallenge2/src/dev/lpa/Main.java
+++ LinkedListChallenge2/src/dev/lpa/Main.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 record Place(String place, int distance) {
     @Override
@@ -11,6 +12,7 @@ record Place(String place, int distance) {
 
 public class Main {
     public static void main(String[] args) {
+
         LinkedList<Place> placesToVisit = new LinkedList<>();
 
         // Testing 1
@@ -20,16 +22,67 @@ public class Main {
         add(new Place("Saudi", 110), placesToVisit);
 
         System.out.println(placesToVisit);
+
+        // using a list Iterator
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean quitLoop = false;
+        boolean forward = true;
+
+        printMenu();
+
+        while (!quitLoop) {
+            System.out.println("Enter a value");
+            String userInput = scanner.nextLine().toUpperCase().substring(0, 1);
+
+            // if node doesn't have a previous, this means we are at the beginning
+            if (!iterator.hasPrevious()) {
+                System.out.println("First node - No more previous");
+                forward = true;
+            }
+            // if node doesn't have a next, this means we are at the end
+            if (!iterator.hasNext()) {
+                System.out.println("Last node - No more next ");
+                forward = false;
+            }
+
+            switch (userInput) {
+                case "F":
+                    System.out.println("Going forward ...");
+                    if (iterator.hasNext()) System.out.println(iterator.next());
+                    break;
+
+                case "B":
+                    System.out.println("Going backward ...");
+                    if (iterator.hasPrevious()) System.out.println(iterator.previous());
+
+                    break;
+                case "L":
+                    System.out.println(placesToVisit);
+                    break;
+                case "M":
+                    printMenu();
+                    break;
+                default:
+                    quitLoop = true;
+            }
+
+        }
+
+
     }
 
     // Add function, sort entry by distance, and don't allow duplication
     public static void add(Place place, LinkedList<Place> linkedList) {
+
         if (linkedList.contains(place)) {
             System.out.println("Duplication found: " + place);
             return;
         }
 
         // iteration tracker if value less, then insert new place, so the list becomes sorted
+
         int indexTracker = 0;
         for (Place l : linkedList) {
             if (place.distance() < l.distance()) {
@@ -41,4 +94,17 @@ public class Main {
 
         linkedList.add(place);
     }
+
+
+    public static void printMenu() {
+        System.out.println("""
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackwards
+                (L)ist Places
+                (M)enu
+                (Q)uit""");
+    }
+
+
 }
--- LinkedListChallenge2/src/dev/lpa/MorePractice.java
+++ LinkedListChallenge2/src/dev/lpa/MorePractice.java
@@ -1,34 +1,103 @@
 package dev.lpa;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class MorePractice {
     public static void main(String[] args) {
+        LinkedList<Location> location = new LinkedList<>();
+        add(new Location("Bahrain", 135), location);
+        add(new Location("Saudi", 110), location);
+        add(new Location("Qatar", 118), location);
+        System.out.println(location);
 
-        LinkedList<Location> locationToVisitAgain = new LinkedList<>();
-        add(new Location("Bahrain", 110), locationToVisitAgain);
-        add(new Location("Saudi", 135), locationToVisitAgain);
-        add(new Location("Qatar", 0), locationToVisitAgain);
-        System.out.println(locationToVisitAgain);
+
+        // I'm The One
+
+        var iteration = location.listIterator();
+        Scanner scanner = new Scanner(System.in);
+//
+        printMenu();
+
+
+        boolean quitLoop = false;
+
+        while (!quitLoop) {
+            System.out.println("Enter a value");
+            String userInput = scanner.nextLine().toUpperCase().substring(0, 1);
+
+
+            if (!iteration.hasPrevious()) {
+                System.out.println("First Node");
+            }
+            if (!iteration.hasNext()) {
+                System.out.println("Last Node");
+            }
+
+            switch (userInput) {
+
+                // Forward
+                case "F":
+                    if (iteration.hasNext()) {
+                        System.out.println(iteration.next());
+                    }
+                    break;
+                case "B":
+                    if (iteration.hasPrevious()) {
+                        System.out.println(iteration.previous());
+                    }
+                    break;
+                case "M":
+                    printMenu();
+                    break;
+                case "L":
+                    System.out.println(location);
+                    break;
+                default:
+                    System.out.println("Exiting ...");
+                    quitLoop = true;
+                    break;
+            }
+        }
+
+
     }
 
-    public static void add(Location place, LinkedList<Location> list) {
+
+    public static void add(Location location, LinkedList<Location> list) {
+
+        // add method, no duplication and sorted
         for (Location l : list) {
-            if (l.name().equalsIgnoreCase(place.name())) {
+            if (l.name().equalsIgnoreCase(location.name())) {
+                System.out.println("Duplication Found: " + location);
                 return;
             }
         }
 
-        // Addition lowest to highest
+        // since we ensured this is not a duplicant, adding now with sort
         int trackingIndex = 0;
         for (Location l : list) {
-            if (place.distance() < l.distance()) {
-                list.add(trackingIndex, place);
+            if (location.distance() < l.distance()) {
+                list.add(trackingIndex, location);
                 return;
             }
             trackingIndex++;
         }
-        list.add(place);
+        // In case this is the first value
+        list.add(location);
     }
+
+
+    public static void printMenu() {
+        System.out.println("""
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackwards
+                (L)ist Places
+                (M)enu
+                (Q)uit""");
+    }
+
 
 }
