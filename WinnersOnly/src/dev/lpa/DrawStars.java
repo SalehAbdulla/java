0ea73fe9fd0a340c44398e336be9638b1b7686d5
java <salehabdullaict@gmail.com>
2025-04-25 12:05:24 +0300

    I'm The One

--- WinnersOnly/src/dev/lpa/DrawStars.java
+++ WinnersOnly/src/dev/lpa/DrawStars.java
@@ -1,7 +1,41 @@
 package dev.lpa;
 
+import javax.swing.*;
+
 public class DrawStars {
     public static void main(String[] args) {
-
+        drawStars(3, false);
     }
+
+    public static void drawStars(int n, boolean isUpSideDown) {
+        //
+        if (n == -1) {
+            System.out.println("Invalid input");
+            return;
+        }
+        //
+        if (!isUpSideDown) {
+            //
+            for (int i = 0; i <= n; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        } else {
+            //
+            for (int i = 0; i <= n; i++) {
+                //
+                for (int j = n; j > i; j--) {
+                    System.out.print("*");
+                }
+
+                //
+                System.out.println();
+            }
+        }
+    }
+
+
 }
+
