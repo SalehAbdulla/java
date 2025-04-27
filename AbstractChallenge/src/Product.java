e8a112e190a66cf0cfd55d9a1f9d80ee6688aa4b
java <salehabdullaict@gmail.com>
2025-04-27 17:41:48 +0300

    I'm The One

---
+++ AbstractChallenge/src/Product.java
@@ -0,0 +1,15 @@
+public class Product {
+
+    private String productName;
+    protected String productType;
+    private double productPrice;
+
+    public Product(String productName, String productType, double productPrice) {
+        this.productName = productName;
+        this.productType = productType;
+        this.productPrice = productPrice;
+    }
+
+
+
+}
---
+++ AbstractChallenge/src/Store.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class Store {
+    ArrayList<Product> productsForSale;
+
+}
