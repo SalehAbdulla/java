f9af7663f26b24c8f72350c4637f2c5b054c25f7
java <salehabdullaict@gmail.com>
2025-04-29 15:26:03 +0300

    I'm The One

--- AbstractChallenge/src/Main.java
+++ AbstractChallenge/src/Main.java
@@ -1,5 +1,19 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        int qty = 55;
+        double price = 3.15;
+        String type = "Winner";
+        String description = "I'm The One";
+
+        System.out.printf("%2d qty at $%8.2f each, %-15s %-35s %n",
+                qty, price, type, description);
+
+
     }
+
+
+
+
 }
--- AbstractChallenge/src/Store.java
+++ AbstractChallenge/src/Store.java
@@ -1,6 +1,26 @@
+import javax.print.attribute.standard.JobImpressions;
 import java.util.ArrayList;
 
 public class Store {
-    ArrayList<Product> productsForSale;
+    private static ArrayList<ProductForSale> storeProducts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        storeProducts.add(new ArtObject("Oil Painting", 1250,
+                "Impressionistic work by ABF painted in 2010"));
+
+        storeProducts.add(new ArtObject("Sculpture", 2000,
+                "Bronze work"));
+
+
+        listProducts();
+    }
+
+    public static void listProducts() {
+        for (var item : storeProducts) {
+            System.out.println("-".repeat(30));
+            item.showDetails();
+        }
+    }
+
 
 }
