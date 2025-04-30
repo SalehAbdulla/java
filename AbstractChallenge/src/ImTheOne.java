65b697c5d38b5aa2613fbdd10c7b3cf8a2c9174d
java <salehabdullaict@gmail.com>
2025-04-30 13:16:29 +0300

    I'm The One

---
+++ AbstractChallenge/src/ImTheOne.java
@@ -0,0 +1,9 @@
+public class ImTheOne {
+    public static void main(String[] args) {
+        int a[] = new int[10];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i * i;
+            System.out.println(a[i]);
+        }
+    }
+}
