97ab62ed2e9ff3a94065ede74e7200ba9ef6fcd3
Saleh Abdulla <salehabdullaict@gmail.com>
2025-06-18 17:54:11 +0300

    Abstract Recap

---
+++ AbstractRecap/src/Dog.java
@@ -0,0 +1,20 @@
+public class Dog extends Animal {
+
+    public Dog(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+        if (type.equals("Wolf")){
+            System.out.println("Howling! ");
+        } else {
+            System.out.println("Woof! ");
+        }
+    }
+}
---
+++ AbstractRecap/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Wolf", "big", 100);
+        dog.makeNoise();
+    }
+}
