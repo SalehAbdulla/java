9fbd18472cc68e48d942a209bd6fecc176f8e5c5
Saleh Abdulla <salehabdullaict@gmail.com>
2025-11-19 21:51:15 +0300

    recap

---
+++ springbootCourse/3_session/untitled/src/Main.java
@@ -0,0 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        int result = num1 + num2;
+        System.out.println(result);
+
+        // ----------------
+
+        Double marks = 99.0;
+
+        int num3 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854L;
+        float f = 5.8f;
+        double d = 5.8;
+
+        char c = 'a';
+        boolean isTrue = true;
+
+
+        int age = 25;
+        double height = 5.9;
+        String name = "Alex";
+        System.out.println("Name:" + name + ", Age:" + age + "Height:" + height);
+
+        char myChar = 'a';
+        System.out.println(myChar);
+
+        // --------
+
+        int num4 = 0x7E;
+        System.out.println(num4);
+
+
+    }
+}
