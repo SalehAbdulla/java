7e9f9f2f232d8d23157b0dad4c58ce234cc17ea0
saabdulla <nonemask1@gmail.com>
2026-01-26 22:00:09 +0300

    Record vs POJO

---
+++ javaPractice/StaticVariables/src/Dog.java
@@ -0,0 +1,13 @@
+public class Dog {
+
+    private static String name;
+
+    public Dog(String name) {
+        System.out.println("name = " + name);
+    }
+
+    public void printName(){
+        System.out.println("name = " + name);
+    }
+
+}
