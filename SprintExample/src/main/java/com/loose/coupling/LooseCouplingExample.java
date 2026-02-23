58c3bba6580dd350fa1a6bb9305c8b275910dd9a
salehabdulla <salehabdullaict@gmail.com>
2026-02-23 12:55:12 +0300

    User Manager implemented

--- SprintExample/src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ SprintExample/src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -3,17 +3,18 @@ package com.loose.coupling;
 public class LooseCouplingExample {
     public static void main(String[] args) {
         UserDataProvider databaseProvider = new UserDatabaseProvider();
-
-        UserManager userManager = new UserManager(databaseProvider);
+        UserManager dbUserManager = new UserManager(databaseProvider);
 
         WebServiceDataProvider webServiceDataProvider = new WebServiceDataProvider(databaseProvider);
+        UserManager webServiceDataProviderManager = new UserManager(webServiceDataProvider);
 
         MongoDBProvider mongoDBProvider = new MongoDBProvider(databaseProvider);
+        UserManager mongoDBProviderManager = new UserManager(mongoDBProvider);
 
 
-        System.out.println(userManager.getUserDetails());
-        System.out.println(webServiceDataProvider.getUserInfo());
-        System.out.println(mongoDBProvider.getUserInfo());
+        System.out.println(dbUserManager.getUserDetails());
+        System.out.println(webServiceDataProviderManager.getUserDetails());
+        System.out.println(mongoDBProviderManager.getUserDetails());
 
     }
 }
