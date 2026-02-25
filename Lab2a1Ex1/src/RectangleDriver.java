9f7deed9d4cf54c1e69c60eeaf83ef036e7fc587
salehabdulla <salehabdullaict@gmail.com>
2026-02-25 12:09:06 +0300

    CollageLab Solution

---
+++ Lab2a1Ex1/src/RectangleDriver.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RectangleDriver {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Rectangle program that will make your life easier");
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Please insert the width: ");
+        double width = scanner.nextDouble();
+        rectangle.setWidth(width);
+
+        System.out.println("Please insert the length");
+        double length = scanner.nextDouble();
+        rectangle.setLength(length);
+
+        System.out.printf("The Length is: %.2f%n", length);
+        System.out.printf("The Width is: %.2f%n", width);
+
+        System.out.println("The area = " + rectangle.calculateArea());
+        System.out.println("The perimeter = " + rectangle.calculatePerimeter());
+    }
+
+}
