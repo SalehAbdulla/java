ec3d36d08f0683b0fba95404c6eb7c059602d8ee
salehabdulla <salehabdullaict@gmail.com>
2026-03-05 04:03:33 +0300

    Powering annotation, using the context without xml file

---
+++ SpringExample/src/main/java/com/example/component/AppConfig.java
@@ -0,0 +1,10 @@
+package com.example.component;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.component")
+public class AppConfig {
+
+}
