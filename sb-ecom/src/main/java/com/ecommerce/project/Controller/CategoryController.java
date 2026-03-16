ca86308d5f8bdf165ff81eb4b5b595f1a9597faf
salehabdulla <salehabdullaict@gmail.com>
2026-03-16 12:11:05 +0300

    Creating two categories endpoints

--- sb-ecom/src/main/java/com/ecommerce/project/Controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Controller/CategoryController.java
@@ -2,6 +2,9 @@ package com.ecommerce.project.Controller;
 
 
 import com.ecommerce.project.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,8 +12,18 @@ import java.util.List;
 
 @RestController
 public class CategoryController {
-    private List<Category> categoryList = new ArrayList<>();
-    
+    private List<Category> categories= new ArrayList<>();
 
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories(){
+        return categories;
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        categories.add(category);
+
+        return "Category added successfully";
+    }
 
 }
