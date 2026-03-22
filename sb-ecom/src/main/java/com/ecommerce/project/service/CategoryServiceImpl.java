ab252743978c0cf570ad54c32543a988098d9641
salehabdulla <salehabdullaict@gmail.com>
2026-03-22 16:41:53 +0300

    feat: making use of H2 database system, and adding JPA ORM dependency for database management.

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -26,7 +26,7 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public String deleteCategory(long categoryId){
+    public String deleteCategory(long categoryId)  {
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId() == categoryId)
                 .findFirst()
