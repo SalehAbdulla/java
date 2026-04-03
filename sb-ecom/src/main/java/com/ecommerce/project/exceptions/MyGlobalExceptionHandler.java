3ed1aba639b54f15dc40abdb07c00f785125446e
salehabdulla <salehabdullaict@gmail.com>
2026-04-03 20:05:20 +0300

    Recaping: How to create a global Exception interceptor with the use of @RestControllerAdvice and @ExceptionHandler,
    impl: intercepting Exception methodArgumentNotValidException, sending friendly JSON to the user.

--- sb-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ sb-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -24,7 +24,7 @@ public class MyGlobalExceptionHandler {
             response.put(fieldName, message);
         });
 
-        return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
