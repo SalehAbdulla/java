c6cbe8518cbbe107c3e932da84b751918779579d
salehabdulla <salehabdullaict@gmail.com>
2026-07-22 08:40:29 +0300

    feat: update SocialUser model to include cascade option for socialProfile

--- media/src/main/java/com/social/media/models/SocialUser.java
+++ media/src/main/java/com/social/media/models/SocialUser.java
@@ -2,6 +2,7 @@ package com.social.media.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,8 +19,8 @@ public class SocialUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @OneToOne(mappedBy = "socialUser") // its mappedBy, means do not show it in JPA DB
+                                        // Casecade will allow us to insert a new user along with its SocialProfile
+    @OneToOne(mappedBy = "socialUser", cascade = CascadeType.ALL) // its mappedBy, means do not show it in JPA DB.
     private SocialProfile socialProfile;
 
     @OneToMany(mappedBy = "socialUser") // its mappedBy again, do not show it
